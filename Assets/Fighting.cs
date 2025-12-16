259b4aeadb6a48299d914e179e73541c0316df8a
Aymen Djoudi <djoudiaymenbusiness@gmail.com>
2025-12-16 18:19:55 +0100

    Made the Attacking animation work

--- Assets/Fighting.cs
+++ Assets/Fighting.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Fighting : MonoBehaviour
 {
+    [SerializeField] private BoxCollider2D _bc;
+    [SerializeField] private Animator anime;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +15,19 @@ public class Fighting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(1))
+        if (Input.GetMouseButtonDown(0))
         {
             Attack();
         }
+            
     }
     void Attack()
     {
-        
+        anime.SetBool("IsAttacking",true);
+        Debug.Log("Attacked");
+    }
+    void SAttack()
+    {
+        anime.SetBool("IsAttacking",false);
     }
 }
