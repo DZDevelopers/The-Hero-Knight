984eef81174d6c5985a9bd3e5af6e8c458bb212a
Aymen Djoudi <djoudiaymenbusiness@gmail.com>
2025-12-16 17:27:17 +0100

    Fixed the animation movement
    now the player turn in the direction of the movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,3 +1,5 @@
+using System;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -7,6 +9,8 @@ public class Movement : MonoBehaviour
     [SerializeField]private float jumpStrength = 5f;
     [SerializeField]private float cyoteTime = 0.15f;
     [SerializeField]private float jumpBufferTime = 0.5f;
+    [SerializeField]private Animator anime;
+    private int facingDir = 1;
     private float jumpBufferCounter;
 
     private float cyoteTimeCounter;
@@ -41,18 +45,35 @@ public class Movement : MonoBehaviour
             Jump();
             cyoteTimeCounter = 0f;
         }
-
+        
     }
 
     void FixedUpdate()
     {
         float moveX = Input.GetAxis("Horizontal");
         _rb.velocity = new Vector2(moveX * speed, _rb.velocity.y);
+        float Horizontal =Input.GetAxis("Horizontal");
+        if (Horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+        if (Horizontal > 0 && transform.localScale.x < 0)
+        {
+            Flip();
+        }
+        anime.SetFloat("Horizontal", Math.Abs(Input.GetAxis("Horizontal")));
+        anime.SetFloat("Vertical", _rb.velocity.y);
+  
     }
     void Jump()
     {
         _rb.velocity = new Vector2(_rb.velocity.x, 0f);
         _rb.AddForce(Vector2.up * jumpStrength, ForceMode2D.Impulse);
+    }
+    void Flip()
+    {
+        facingDir *= -1;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
